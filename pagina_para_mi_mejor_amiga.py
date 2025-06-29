4d8979f1353cdcadd96cbf058844ebb708001429
luna-2026 <zadalonsohc@gmail.com>
2025-06-29 13:30:14 -0600

    Add files via upload

---
+++ pagina_para_mi_mejor_amiga.py
@@ -0,0 +1,45 @@
+
+import streamlit as st
+import base64
+
+# Configuración de la página
+st.set_page_config(page_title="Para mi mejor amiga 💖", layout="centered")
+
+# Título principal
+st.title("🌸 Para mi mejor amiga 🌸")
+
+# Reproductor de música con autoplay (YouTube embed con autoplay activado)
+st.markdown("""
+<iframe width="0" height="0" src="https://www.youtube.com/embed/wD1LqNqJ-kY?autoplay=1" 
+frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+""", unsafe_allow_html=True)
+
+# Carta personalizada
+st.markdown("""
+### 💌 Una carta para ti
+
+> Querida amiga,  
+> Quería dedicarte este pequeño rincón en la web para recordarte lo especial que eres para mí.  
+> Cada momento compartido, cada risa, cada conversación ha dejado una huella imborrable en mi corazón.  
+> Gracias por ser tú, por estar siempre, por tu luz y tu cariño.  
+> Espero que esta página te saque una sonrisa y te recuerde cuánto te aprecio.  
+>   
+> Con todo mi cariño,  
+> Tu mejor amigo 💖
+""")
+
+# Galería de imágenes
+st.markdown("### 🐾 Galería de Gatitos y Perritos")
+st.write("Puedes subir tus imágenes favoritas para verlas aquí:")
+
+uploaded_images = st.file_uploader("Sube tus imágenes aquí", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+
+if uploaded_images:
+    for img in uploaded_images:
+        st.image(img, use_column_width=True)
+else:
+    st.info("Aún no se han subido imágenes. ¡Sube algunas para llenar esta galería de ternura!")
+
+# Mensaje final
+st.markdown("---")
+st.markdown("Hecho con mucho cariño 💖")
